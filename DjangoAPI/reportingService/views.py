22ee61b1f3d04bf2463db3ff78c037a3055ec965
gerasmark <gerasmark@hotmail.gr>
2023-09-03 18:44:41 +0300

    First api endpoint to get all sensor readings

--- DjangoAPI/reportingService/views.py
+++ DjangoAPI/reportingService/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.view.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from reportingService.models import Sensor, SensorReading
+from reportingService.serializers import SensorSerializer, SensorReadingSerializer
 
 # Create your views here.
+
+@csrf_exempt
+def get_sensors(request):
+    
