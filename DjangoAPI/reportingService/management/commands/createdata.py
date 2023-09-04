09232f6584a4adb08f0b365e19f73735de66708f
gerasmark <gerasmark@hotmail.gr>
2023-09-04 10:55:31 +0300

    changed model attribute fields(readingDate,time)

--- DjangoAPI/reportingService/management/commands/createdata.py
+++ DjangoAPI/reportingService/management/commands/createdata.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 from faker import Faker
 import random
 from reportingService.models import Sensor, SensorReading
-from datetime import datetime
 
 class Command(BaseCommand):
     help = 'Create fake data for our database'
@@ -16,7 +15,7 @@ class Command(BaseCommand):
         sensor_types = ['temperature', 'humidity', 'acoustic']
         for _ in range(50):  
             sensor = Sensor(
-                sensorId=fake.unique.random_int(min=1000, max=9999), 
+                sensorId=fake.unique.random_int(min=10000, max=99999), 
                 type=random.choice(sensor_types),
                 vendorName=fake.company(),
                 vendorEmail=fake.email(),
@@ -24,25 +23,21 @@ class Command(BaseCommand):
                 location=fake.city()
             )
             sensor_data.append(sensor)
-        Sensor.objects.bulk_create(sensor_data)
+        # Sensor.objects.bulk_create(sensor_data)
         sensor_reading_data = []
         for sensor in Sensor.objects.all():
             for _ in range(5):  
                 sensor_reading = SensorReading(
-                    # sensorId=sensor,
-                    # readingType=sensor.type,
-                    # readingValue=fake.pydecimal(min_value=0, max_value=100, right_digits=2),
-                    # readingDate=fake.date_this_century(),
-                    # description=fake.sentence(),
-                    # time=fake.time(pattern='%H:%M:%S'),
-                    Id = fake.unique.random_int(min=1, max=1000),
-                    sensorId=sensor,
+                    Id = fake.unique.random_int(min=10000, max=99999),
+                    sensorId=sensor.sensorId,
                     readingType=sensor.type,
                     readingValue=fake.pydecimal(min_value=0, max_value=100, right_digits=2),
-                    readingDate=fake.date_between(start_date='-30d', end_date='today').strftime('%Y-%m-%d'),
+                    readingDate=fake.date(),  
                     description=fake.sentence(),
-                    time=fake.time(pattern='%H:%M:%S')
+                    time=fake.time()
                 )
+                print(sensor_reading.readingDate)
+                print(sensor_reading.time)
                 sensor_reading_data.append(sensor_reading)
-        SensorReading.objects.bulk_create(sensor_reading_data)
+        # SensorReading.objects.bulk_create(sensor_reading_data)
 
--- DjangoAPI/reportingService/models.py
+++ DjangoAPI/reportingService/models.py
@@ -19,13 +19,19 @@ class Sensor(models.Model):
         return f"Sensor: {self.sensorId} "
 
 class SensorReading(models.Model):
-    Id = models.AutoField(primary_key=True)
+    SENSOR_TYPES = [
+        ('temperature', 'Temperature'),
+        ('humidity', 'Humidity'),
+        ('acoustic', 'Acoustic'),
+    ]
+
+    Id = models.CharField(max_length=50, primary_key=True)
     sensorId = models.CharField(max_length=50)
-    readingType = models.CharField(max_length=50)
+    readingType = models.CharField(max_length=20, choices=SENSOR_TYPES)
     readingValue = models.DecimalField(max_digits=10, decimal_places=2)
-    readingDate = models.DateField()
+    readingDate = models.CharField(max_length=10)  #DateField()
     description = models.TextField()
-    time = models.TimeField()
+    time = models.CharField(max_length=9)  #TimeField()
 
     def __str__(self):
         return f"Sensor Reading Id {self.Id} - Sensor: {self.sensorId.sensorId}"
