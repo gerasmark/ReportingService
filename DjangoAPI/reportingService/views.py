0e60683619afb966088018c1b3007a9563f7ef2a
gerasmark <gerasmark@hotmail.gr>
2023-09-04 12:59:22 +0300

    post request that filters with type and location

--- DjangoAPI/reportingService/views.py
+++ DjangoAPI/reportingService/views.py
@@ -5,22 +5,31 @@ from django.http.response import JsonResponse
 from django.core.paginator import Paginator
 from reportingService.models import Sensor, SensorReading
 from reportingService.serializers import SensorSerializer, SensorReadingSerializer
+import json
 
 # Create your views here.
 
 @csrf_exempt
 def sensorReadings(request):
-    # if request.method == 'POST':
-    #     data = JSONParser().parse(request)
-    #     serializer = SensorReadingSerializer(data=data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return JsonResponse(serializer.data, status=201)
-    #     else:
-    #         return JsonResponse(serializer.errors, status=400)
-    # else:
-    #     return JsonResponse(status=405)
-    if request.method == 'GET':
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        type = body['type']
+        location = body['location']
+        time = body['time']
+        sensorReadings = SensorReading.objects.all()
+        sensor = Sensor.objects.all()
+        if type:
+            sensor = sensor.filter(type=type)
+        if location:
+            sensor = sensor.filter(location=location)
+        # sensorReadings = sensorReadings.filter(sensorId__in=sensor)    
+        # if time:
+        # query = query.filter(time=time)
+        serializer = SensorSerializer(sensor, many=True)
+        return JsonResponse(serializer.data, status=200, safe=False)
+
+    elif request.method == 'GET':
         sensorReadings = SensorReading.objects.all()
         serializer = SensorReadingSerializer(sensorReadings, many=True)
         page_size = 10
