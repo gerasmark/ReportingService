c1ff365016084493ee6624d4f8669575f62efd2c
gerasmark <gerasmark@hotmail.gr>
2023-09-05 12:56:07 +0300

    create a simple unit test

--- DjangoAPI/reportingService/tests.py
+++ DjangoAPI/reportingService/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from rest_framework.test import APIClient
+from rest_framework import status
+from reportingService.models import SensorReading, Sensor  
+from reportingService.views import sensorMetrics, sensorReadings  
+from datetime import datetime, timedelta
 # Create your tests here.
+
+class first(TestCase):
+    def test(self):
+        self.assertEqual(True, True)
+    
