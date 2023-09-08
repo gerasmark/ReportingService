632aa2d2e25bceb87ba1abd99b202b3cdff65c62
gerasmark <gerasmark@hotmail.gr>
2023-09-08 13:48:21 +0300

    created navigation bar for frontend

---
+++ frontend/pages/api/postsensorMetrics.tsx
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+    const { message } = req.body;
+  try {
+    const response = 
+    await fetch('http://djangoapi:8000/sensorMetrics/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message })
+      });
+    const count = await response.json();
+    res.status(200).json(count)
+    }catch (error) {
+    res.status(500).json({ message: 'Error fetching data' });
+  }
+}
