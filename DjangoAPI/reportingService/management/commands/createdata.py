a13e8f2cef8a057351dfd20d07b06a6a5d82a631
gerasmark <gerasmark@hotmail.gr>
2023-09-14 13:12:48 +0300

    made restrictions on createdata tohaveno conflicts

--- DjangoAPI/reportingService/management/commands/createdata.py
+++ DjangoAPI/reportingService/management/commands/createdata.py
@@ -10,7 +10,8 @@ class Command(BaseCommand):
         print('Creating fake data for our database...')
         fake = Faker()
         Faker.seed(123)
-
+        database_sensor = Sensor.objects.all()
+        database_sensor_reading = SensorReading.objects.all()
         sensor_data = []
         sensor_types = ['temperature', 'humidity', 'acoustic']
         for _ in range(100):  
@@ -22,7 +23,11 @@ class Command(BaseCommand):
                 description=fake.sentence(),
                 location=fake.city()
             )
-            sensor_data.append(sensor)
+            empty = database_sensor.filter(sensorId=sensor.sensorId)
+            print(empty)
+            if len(empty) == 0:
+                sensor_data.append(sensor)
+
         Sensor.objects.bulk_create(sensor_data)
         sensor_reading_data = []
         for sensor in Sensor.objects.all():
@@ -37,11 +42,13 @@ class Command(BaseCommand):
                     Id = fake.unique.random_int(min=10000, max=99999),
                     sensorId=sensor.sensorId,
                     readingType=sensor.type,
-                    readingValue=number, #fake.pydecimal(min_value=0, max_value=100, right_digits=2),
+                    readingValue=number, 
                     readingDate=fake.date(),  
                     description=fake.sentence(),
                     time=fake.time()
                 )
-                sensor_reading_data.append(sensor_reading)
+                empty = database_sensor_reading.filter(Id=sensor_reading.Id)
+                if len(empty) == 0:
+                    sensor_reading_data.append(sensor_reading)
         SensorReading.objects.bulk_create(sensor_reading_data)
 
