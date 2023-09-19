e45694eda3c5995f9912a051de12101468002964
gerasmark <gerasmark@hotmail.gr>
2023-09-19 20:58:29 +0300

    added makemigrations to entrypoint

--- DjangoAPI/entrypoint.sh
+++ DjangoAPI/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
-python3 manage.py createdata
+# python3 manage.py createdata
 python3 manage.py runserver 0.0.0.0:8000
