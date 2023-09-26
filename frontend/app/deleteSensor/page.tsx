24efa74a9d8577a98d04a2ba9c88503fe668a0e5
gerasmark <gerasmark@hotmail.gr>
2023-09-26 13:46:01 +0300

    added page for deleting sensor and sen readings

---
+++ frontend/app/deleteSensor/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import AppBar from '../AppBar';
+import LoadingButton from '@mui/lab/LoadingButton';
+
+function DeleteSensor() {
+    const [sensorId, setSensorId] = useState('');
+    const [responseMessage, setResponseMessage] = useState(' ');
+    
+    const handleDelete = async (sensorId) => {
+      try {
+        console.log(sensorId)
+        const response = await fetch(`http://localhost:8000/deleteSensor/${sensorId}/`, {
+          method: 'DELETE',
+        });
+  
+        if (response.status === 200) {
+          setResponseMessage('Sensor deleted successfully');
+        } else {
+          setResponseMessage('Failed to delete sensor');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        setResponseMessage('An error occurred');
+      }
+    };
+
+    return (
+      <div>
+        <AppBar />
+        <main style={{ marginTop: '20px' }}></main>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <InputLabel>Sensor Id</InputLabel>
+          <TextField
+            value={sensorId}
+            onChange={(e) => setSensorId(e.target.value)}
+          />
+        <LoadingButton
+            variant="contained"
+            color="primary"
+            onClick={() => handleDelete(sensorId)}
+        >
+          Delete Sensor
+        </LoadingButton>
+        <p>{responseMessage}</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default DeleteSensor;
+  
---
+++ frontend/app/deleteSensorReading/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import AppBar from '../AppBar';
+import LoadingButton from '@mui/lab/LoadingButton';
+
+function DeleteSensorReading() {
+    const [Id, setId] = useState('');
+    const [responseMessage, setResponseMessage] = useState(' ');
+    
+    const handleDelete = async (Id) => {
+      try {
+        console.log(Id)
+        const response = await fetch(`http://localhost:8000/deleteSensorReading/${Id}/`, {
+          method: 'DELETE',
+        });
+  
+        if (response.status === 200) {
+          setResponseMessage('Sensor reading deleted successfully');
+        } else {
+          setResponseMessage('Failed to delete sensor reading');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        setResponseMessage('An error occurred');
+      }
+    };
+
+    return (
+      <div>
+        <AppBar />
+        <main style={{ marginTop: '20px' }}></main>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <InputLabel>Sensor Reading Id</InputLabel>
+          <TextField
+            value={Id}
+            onChange={(e) => setId(e.target.value)}
+          />
+        <LoadingButton
+            variant="contained"
+            color="primary"
+            onClick={() => handleDelete(Id)}
+        >
+          Delete Sensor Reading
+        </LoadingButton>
+        <p>{responseMessage}</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default DeleteSensorReading;
+  
