7ca5768dbb33d90d1193c25a8f1805185d64ad71
gerasmark <gerasmark@hotmail.gr>
2023-09-29 17:19:56 +0300

    added jenkis dockerfile

--- jenkins.sh
+++ jenkins.sh
@@ -1,2 +1,2 @@
 !#/bin/sh
-docker run -p 8080:8080 -p 50000:50000 --restart=on-failure -d -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk17
+docker run -p 8080:8080 -p 50000:50000 --restart=on-failure -v /var/run/docker.sock:/var/run/docker.sock -d -v jenkins_home:/var/jenkins_home 46211
