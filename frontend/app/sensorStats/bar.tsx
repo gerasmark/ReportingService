097deab10185874d6e2cde675ff78f1244127dbb
gerasmark <gerasmark@hotmail.gr>
2023-10-11 11:37:50 +0300

    bar plot ready!!!

--- frontend/app/sensorStats/bar.tsx
+++ frontend/app/sensorStats/bar.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { ResponsiveBar } from '@nivo/bar';
 
-function BarPlot({ data, xIndex, yIndex }) {
+function BarPlot({ data, xIndex, xTitle }) {
   return (
     <div style={{ height: '300px', width: '400px' }}>
       <ResponsiveBar
         data={data}
-        keys={['value']} // Specify the data keys to use for the bars
-        indexBy= xIndex // Specify the data key to use for the x-axis labels
-        margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
+        keys={['count']} 
+        indexBy= {xIndex}
         padding={0.3}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
         colors={{ scheme: 'category10' }}
         borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
         axisTop={null}
@@ -18,16 +18,19 @@ function BarPlot({ data, xIndex, yIndex }) {
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
+          legend: xTitle
         }}
         axisLeft={{
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: yIndex, // Specify the y-axis label
+          legend: 'count', 
           legendPosition: 'middle',
           legendOffset: -40,
         }}
-        enableLabel={false} // Set to true to display labels on the bars
+        enableLabel={true}
+        width={2000} 
+        height={400}
       />
     </div>
   );
