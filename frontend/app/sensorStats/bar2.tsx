81a0bdbdb19cb864889cdbc70de32befc6869457
gerasmark <gerasmark@hotmail.gr>
2023-10-11 12:18:03 +0300

    added second bar component

---
+++ frontend/app/sensorStats/bar2.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ResponsiveBar } from '@nivo/bar';
+
+function BarPlot({ data, xIndex, xTitle }) {
+  return (
+    <div style={{ height: '300px', width: '400px' }}>
+      <ResponsiveBar
+        data={data}
+        keys={['count']} 
+        indexBy= {xIndex}
+        padding={0}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        colors={{ scheme: 'nivo' }}
+        borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: xTitle
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'count', 
+          legendPosition: 'middle',
+          legendOffset: -40,
+        }}
+        enableLabel={true}
+        width={2000} 
+        height={400}
+      />
+    </div>
+  );
+}
+
+export default BarPlot;
